3bcc28162e6d6a141e284fd991eb6bbd770ae3a3
chiang9 <jchiang1225@gmail.com>
2020-10-30 17:03:22 +0800

    init push

---
+++ Assets/Scripts/MapReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class MapReader : MonoBehaviour
+{
+    [Tooltip("The resouce file for OSM data")]
+    public string resourceFile;
+
+    void Start()
+    {
+        var txtAsset = Resources.Load<TextAsset>(resourceFile);
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(txtAsset.text);
+
+        SetBounds(doc.SelectSingleNode("/osm/bounds"));
+        SetNodes(doc.SelectNodes("/osm/node"));
+        GetWays(doc.SelectNodes("/osm/way"));
+
+
+    }
+
+    private void GetWays(XmlNodeList xmlNodeList)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SetNodes(XmlNodeList xmlNodeList)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SetBounds(XmlNode xmlNode)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
